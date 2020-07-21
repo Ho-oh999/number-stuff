2101f3181cb982f8e9d3a09f0855263d7b855604
boko999 <ho-oh999@hotmail.com>
2020-07-21 17:42:34 +0000

    more stuff

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ comp_num = random.randint(0, 10)
 tries = 3
 win = False
 play = True
+
+
+while play == True:
+  while trise > 0:
+    print()
+    player_num = input("enter a number between 0-10")
+    player_num = float( player_num)
+    if  player_num <0 or  player_num > 10:
+      print ("bad number")
+      break
